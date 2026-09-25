0d58e08c10a914d1dbc2281288ced7039f5e46c8
Romesa <romesa2607@gmail.com>
2026-09-25 16:46:35 +0200

    classe

--- Activitat 2/RotX.java
+++ Activitat 2/RotX.java
@@ -1,12 +1,8 @@
 public class RotX {
 
-    static char[] minuscules = {'a','á','à','b','c','ç','d','e','é','è','f','g','h','i','í','ï',
-                                 'j','k','l','m','n','o','ó','ò','p','q','r','s','t','u','ù','ú',
-                                 'ü','v','w','x','y','z'};
+    static char[] minuscules = {'a', 'á', 'à', 'b', 'c', 'ç', 'd', 'e', 'é', 'è', 'f', 'g', 'h', 'i', 'í', 'ï', 'j', 'k', 'l', 'm', 'n', 'o', 'ó', 'ò', 'p', 'q', 'r', 's', 't', 'u', 'ù', 'ú', 'ü', 'v', 'w', 'x', 'y', 'z'};
+    static char[] majuscules = {'A', 'Á', 'À', 'B', 'C', 'Ç', 'D', 'E', 'É', 'È', 'F', 'G', 'H', 'I', 'Í', 'Ï', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ò', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ù', 'Ú', 'Ü', 'V', 'W', 'X', 'Y', 'Z'};
 
-    static char[] majuscules = {'A','Á','À','B','C','Ç','D','E','É','È','F','G','H','I','Í','Ï',
-                                 'J','K','L','M','N','O','Ó','Ò','P','Q','R','S','T','U','Ù','Ú',
-                                 'Ü','V','W','X','Y','Z'};
 
     public static String xifraRotX(String cadena, int desplacament) {
         String resultat = "";
@@ -55,6 +51,24 @@ public class RotX {
         }
     }
 
+    public static void main(String[] args) {
+        System.out.println("Xifrat");
+        System.out.println("------");
+        System.out.println("(0)-ABC => " + xifraRotX("ABC", 0));
+        System.out.println("(2)-XYZ => " + xifraRotX("XYZ", 2));
+        System.out.println("(4)-Hola, Mr. calçot => " + xifraRotX("Hola, Mr. calçot", 4));
+        System.out.println("(6)-Perdó, per tu què és? => " + xifraRotX("Perdó, per tu què és?", 6));
+
+        System.out.println();
+        System.out.println("Desxifrat");
+        System.out.println("---------");
+        System.out.println("(0)ABC => " + desxifraRotX("ABC", 0));
+        System.out.println("(2)ZAÁ => " + desxifraRotX("ZAÁ", 2));
+        System.out.println("(4)Ïqoc, Óú. écoèqü => " + desxifraRotX("Ïqoc, Óú. écoèqü", 4));
+        System.out.println("(6)Úiüht, úiü wx ùxì áv? => " + desxifraRotX("Úiüht, úiü wx ùxì áv?", 6));
+
+        System.out.println();
+        forcaBrutaRotX("Úiüht, úiü wx ùxì ív?");
     
     }
 }
